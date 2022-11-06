08c734f53ce39c581c3f8f6b19e78fef0b468f64
notanennx <41900703+notanennx@users.noreply.github.com>
2022-11-06 16:14:43 +0400

    more work on planting

--- Assets/Source/Scripts/Systems/PlantingSystem.cs
+++ Assets/Source/Scripts/Systems/PlantingSystem.cs
@@ -12,37 +12,52 @@ public class PlantingSystem : MonoBehaviour
     [SerializeField, BoxGroup("Main")] private ScriptablePlant[] allowedPlants;
     [SerializeField, BoxGroup("Main")] private CinemachineVirtualCamera virtualCamera;
 
+    [SerializeField, BoxGroup("User Interface")] private GameObject plantingItemPrefab;
+    [SerializeField, BoxGroup("User Interface")] private RectTransform plantingMenuContent;
     [SerializeField, BoxGroup("User Interface")] private RectTransform plantingMenuTransform;
 
-    // Hidden
+    // Events
+    private PlantEvent plantEvent;
     private PlantMenuEvent plantMenuEvent;
 
     // Awaking
     private void Awake()
     {
         // Get
+        plantEvent = Signals.Get<PlantEvent>();
         plantMenuEvent = Signals.Get<PlantMenuEvent>();
 
         // Subscribe
-        plantMenuEvent.AddListener(OnPlantClicked);
+        plantEvent.AddListener(OnItemClicked);
+        plantMenuEvent.AddListener(OnTileClicked);
     }
 
     // Destroy
     private void OnDestroy()
     {
-        plantMenuEvent.RemoveListener(OnPlantClicked);
+        plantEvent.RemoveListener(OnItemClicked);
+        plantMenuEvent.RemoveListener(OnTileClicked);
     }
 
-    // Handles clicks on plant
-    private void OnPlantClicked(PlanterComponent inputPlanter)
+    // Handles clicks on tile
+    private void OnTileClicked(PlanterComponent inputPlanter)
     {
         // Set
         virtualCamera.m_Follow = inputPlanter.transform;
         virtualCamera.m_Priority = 100;
-        Debug.Log("Focusing camera!");
 
-        // Open
+        // Menu
         ShowPlantingMenu();
+    }
+
+    // Handles clicks on plant
+    private void OnItemClicked(PlantingItemUI inputPlantItem)
+    {
+        // Set
+        virtualCamera.m_Priority = 0;
+
+        // Menu
+        HidePlantingMenu();
     }
 
     // Opens a planting menu
@@ -50,5 +65,40 @@ public class PlantingSystem : MonoBehaviour
     {
         // Set
         GameSystem.IsMenuOpen = true;
+        plantingMenuTransform.gameObject.SetActive(true);
+
+        // Fillup
+        FillupContents();
+    }
+
+    // Hides a planting menu
+    private void HidePlantingMenu()
+    {
+        // Set
+        GameSystem.IsMenuOpen = false;
+        plantingMenuTransform.gameObject.SetActive(false);
+    }
+
+    // Cleans planting items contents
+    private void CleanupContents()
+    {
+        PlantingItemUI[] plantingItems = plantingMenuContent.GetComponentsInChildren<PlantingItemUI>();
+        foreach (PlantingItemUI plantingItemUI in plantingItems)
+            Destroy(plantingItemUI.gameObject);
+    }
+
+    // Fills up contents
+    private void FillupContents()
+    {
+        // Clean
+        CleanupContents();
+
+        // Loop and create
+        foreach (ScriptablePlant itemPlant in allowedPlants)
+        {
+            // Create
+            PlantingItemUI newItem = Instantiate(plantingItemPrefab, plantingMenuContent).GetComponent<PlantingItemUI>();
+                newItem.SetPlant(itemPlant);
+        }
     }
 }
---
+++ Assets/Source/Scripts/UI/PlantingItemUI.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Supyrb;
+using Cinemachine;
+using NaughtyAttributes;
+
+public class PlantingItemUI : MonoBehaviour
+{
+    // Vars
+    [SerializeField, BoxGroup("Main")] private Image iconImage;
+    [SerializeField, BoxGroup("Main")] private ScriptablePlant plantToUse;
+
+    // Events
+    private PlantEvent plantEvent;
+
+    // Hidden
+    private Button mainButton;
+    private PlanterComponent planterComponent;
+    
+    // Setters
+    public void SetPlanter(PlanterComponent inputPlanter) => planterComponent = inputPlanter;
+
+    // Awaking
+    private void Awake()
+    {
+        // Get
+        mainButton = GetComponent<Button>();
+        plantEvent = Signals.Get<PlantEvent>();
+
+        // Subscribe
+        mainButton.onClick.AddListener(delegate{OnButtonClicked();});
+    }
+
+    // Destroy
+    private void OnDestroy()
+    {
+        // Unsubscribe
+        mainButton.onClick.RemoveAllListeners();
+    }
+
+    // Sets a plant to item
+    public void SetPlant(ScriptablePlant inputPlant)
+    {
+        // Set
+        plantToUse = inputPlant;
+
+        // Icon
+        iconImage.sprite = inputPlant.SpriteIcon;
+    }
+
+    // Button clicked
+    private void OnButtonClicked()
+    {
+        // Dispatch
+        plantEvent.Dispatch(this);
+
+        // Debugging
+        Debug.Log("Plant button clicked!");
+    }
+}
